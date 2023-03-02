f68eaf8781ee6287b2ba61c6e9d3d3654d50155c
Your Name <abdirimov110@gmail.com>
2023-03-02 17:40:13 +0500

    fkjdh

--- app.js
+++ app.js
@@ -3,25 +3,23 @@ let bolish = prompt("10 / 2 =");
 let kopaytirish =prompt("5 * 2 =");
 const small = function (a, b, c) {
   if (a == 10) {
-    alert("true");
+    alert("true 5 + 5 = 10");
   } else if (a == 0) {
     alert("Qatorni to'ldiring!");
   }else if(a <= 9 && a >= 10){
-    alert("false");
-  }
-  else if (b == 5) {
-    alert("true");
+    alert("false 5 + 5 = 10");
+  }else if (b == 5) {
+    alert("true 10 / 2 = 5");
   } else if (b == 0) {
     alert("Qatorni to'ldiring!");
   }else if(b <= 4 && b >= 6){
-    alert("false");
-  }
-  else if(c == 10){
-    alert("true")
+    alert("false 10 / 2 = 5");
+  }else if(c == 10){
+    alert("true 5 * 2 = 10")
   }else if(c == 0){
     alert("Qatorni to'ldiring!")
   }else{
-    alert("false")
+    alert("false 5 * 2 = 10")
   }
 };
 alert(small(son));
